a6771d5ea2f6d1e0579cb856d619d9d7495b3f63
Shane <shanemcdermott@unomaha.edu>
2017-11-20 04:47:58 -0600

    Added JSON Export.

---
+++ Source/Genesis/Public/Maps/GenesisMapGenerator.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Maps/IslandMapGenerator.h"
+#include "GenesisMapGenerator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENESIS_API AGenesisMapGenerator : public AIslandMapGenerator
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/Genesis/Public/PointGenerators/PoissonPointGenerator.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Maps/PointGenerators/PointGenerator.h"
+#include "PoissonPointGenerator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENESIS_API UPoissonPointGenerator : public UPointGenerator
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
